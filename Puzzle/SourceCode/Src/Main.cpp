499aebf84d75658ac2b74dd77f7a1025ffe3f3a4
Matt L. <mql>
2025-02-21 09:03:37 +0800

    update Main.cpp

--- Puzzle/SourceCode/Src/Main.cpp
+++ Puzzle/SourceCode/Src/Main.cpp
@@ -7,89 +7,124 @@
 // AUTHOR:             mql
 ///////////////////////////////////////////////////////////////////
 
+// includes
+//------------------------------
 #include "CommonClass.h"
 #include "LessonX.h"
 
-//=================================================================================
+//===================================================================
+// NAME        : WinMain
 //
-// WinMain - Windows 程序入口函数
-//
-//=================================================================================
+// DESCRIPTION : Windows 程序入口函数。
+// AUTHOR      : mql
+// DATE        : 250221
+//===================================================================
 int PASCAL WinMain(HINSTANCE hInstance,
                    HINSTANCE hPrevInstance,
                    LPSTR     lpCmdLine,
                    int       nCmdShow)
 {
     // 初始化游戏引擎
-    if (!CSystem::InitGameEngine(hInstance, lpCmdLine))
-        return 0;
-
+    if (!CSystem::InitGameEngine(hInstance, lpCmdLine)) return 0;
     // 设置窗口标题
-    CSystem::SetWindowTitle("15 Puzzle @mql");
-
+    CSystem::SetWindowTitle("【15 Puzzle】by FunCode @mql");
     // 引擎主循环，处理屏幕图像刷新等工作
-    while (CSystem::EngineMainLoop())
-    {
+
+    while (CSystem::EngineMainLoop()) {
         // 获取两次调用之间的时间差，传递给游戏逻辑处理
         float fTimeDelta = CSystem::GetTimeDelta();
-
         // 执行游戏主循环
         g_GameMain.GameMainLoop(fTimeDelta);
     }
 
     // 关闭游戏引擎
     CSystem::ShutdownGameEngine();
+
     return 0;
 }
 
-//=================================================================================
+//===================================================================
+// NAME        : OnMouseMove
 //
-// 鼠标输入处理
-//
-//=================================================================================
+// DESCRIPTION : 监听鼠标移动。
+// AUTHOR      : mql
+// DATE        : 250221
+//===================================================================
 void CSystem::OnMouseMove(const float fMouseX, const float fMouseY)
 {
-    // 可在此添加游戏需要的响应函数
+
 }
 
+//===================================================================
+// NAME        : OnMouseClick
+//
+// DESCRIPTION : 监听鼠标点击。
+// AUTHOR      : mql
+// DATE        : 250221
+//===================================================================
 void CSystem::OnMouseClick(const int iMouseType, const float fMouseX, const float fMouseY)
 {
-    // 处理鼠标点击
+    // 响应鼠标点击事件
     g_GameMain.OnMouseClick(iMouseType, fMouseX, fMouseY);
 }
 
+//===================================================================
+// NAME        : OnMouseUp
+//
+// DESCRIPTION : 监听鼠标抬起。
+// AUTHOR      : mql
+// DATE        : 250221
+//===================================================================
 void CSystem::OnMouseUp(const int iMouseType, const float fMouseX, const float fMouseY)
 {
-    // 可在此添加游戏需要的响应函数
+
 }
 
-//=================================================================================
+//===================================================================
+// NAME        : OnKeyDown
 //
-// 键盘输入处理
-//
-//=================================================================================
+// DESCRIPTION : 监听键盘按键。
+// AUTHOR      : mql
+// DATE        : 250221
+//===================================================================
 void CSystem::OnKeyDown(const int iKey, const bool bAltPress, const bool bShiftPress, const bool bCtrlPress)
 {
-    // 处理键盘按下事件
-    g_GameMain.OnKeyDown(iKey, bAltPress, bShiftPress, bCtrlPress);
+    // 响应键盘按键事件
+	g_GameMain.OnKeyDown(iKey, bAltPress, bShiftPress, bCtrlPress);
 }
 
+//===================================================================
+// NAME        : OnKeyUp
+//
+// DESCRIPTION : 监听键盘抬起。
+// AUTHOR      : mql
+// DATE        : 250221
+//===================================================================
 void CSystem::OnKeyUp(const int iKey)
 {
-    // 可在此添加游戏需要的响应函数
+
 }
 
-//=================================================================================
+//===================================================================
+// NAME        : OnSpriteColSprite
 //
-// 精灵碰撞检测
-//
-//=================================================================================
+// DESCRIPTION : 监听精灵碰撞。
+// AUTHOR      : mql
+// DATE        : 250221
+//===================================================================
 void CSystem::OnSpriteColSprite(const char* szSrcName, const char* szTarName)
 {
-    // 可在此添加游戏需要的响应函数
+
 }
 
+//===================================================================
+// NAME        : OnSpriteColWorldLimit
+//
+// DESCRIPTION : 监听精灵碰撞世界边界。
+// AUTHOR      : mql
+// DATE        : 250221
+//===================================================================
 void CSystem::OnSpriteColWorldLimit(const char* szName, const int iColSide)
 {
-    // 可在此添加游戏需要的响应函数
+
 }
